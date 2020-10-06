1c42ead2d0136b73524649e8bc31bc5e540c36db
kibukj <kibuzhj@gmail.com>
2020-10-06 14:45:42 -0400

    Use shared_scripts

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,16 +2,17 @@ fx_version 'adamant'
 game 'common'
 
 description 'PoodleChat'
-version '2.0.1'
+version '2.0.2'
+
+shared_scripts {
+	'config.lua',
+	'emoji.lua'
+}
 
 client_scripts {
-	'config.lua',
-	'emoji.lua',
 	'client.lua'
 }
 
 server_scripts {
-	'config.lua',
-	'emoji.lua',
 	'server.lua'
 }
